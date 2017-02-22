139a6e3710b26c5534dfff49616b47ca130e2a71
wangchenghui <Wang123@>
2017-02-22 10:22:27 +0800

    add casebase

---
+++ src/test/java/com/CaseBase.java
@@ -0,0 +1,17 @@
+package com;
+
+import com.hivin.WebApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by
+ *
+ * @auth:hivin
+ * @date:17/2/22
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {WebApplication.class})
+public class CaseBase {
+}
--- src/test/java/com/SpringBootTest.java
+++ src/test/java/com/SpringBootTest.java
@@ -1,18 +1,12 @@
 package com;
 
-import com.hivin.WebApplication;
-import com.hivin.configure.Master;
-import com.hivin.dao.TestDao;
 import com.hivin.service.ITestService;
 import com.hivin.vo.TestDO;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 /**
@@ -21,10 +15,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @auth:hivin
  * @date:17/1/23
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {WebApplication.class})
+
 @ActiveProfiles(value="dev")
-public class SpringBootTest {
+public class SpringBootTest extends CaseBase{
 
     @Autowired
     @Qualifier("testService")
